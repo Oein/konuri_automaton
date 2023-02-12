223e045ef5aa7a14efeaf82743f21aac15323291
Oein <oein0219@gmail.com>
2023-02-12 20:16:45 +0900

    형형!

--- js/samplerSettings.js
+++ js/samplerSettings.js
@@ -1,17 +1,8 @@
 const sampler_settings = {
   urls: {
-    C4: "c4.mp3",
-    D4: "d4.mp3",
-    E4: "e4.mp3",
-    F4: "f4.mp3",
-    G4: "g4.mp3",
-    A4: "a4.mp3",
-    B4: "b4.mp3",
-    C5: "c5.mp3",
-    D5: "d5.mp3",
-    E5: "e5.mp3",
+    C4: "sample.mp3",
   },
-  baseUrl: "https://oein.github.io/Riano/audio/konuri/",
+  baseUrl: "./",
 };
 
 export default sampler_settings;
